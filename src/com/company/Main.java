c96fb76a21d912c2b2f4fb90b8a01123d0d3635d
Starmikle <88329415+Starmikle@users.noreply.github.com>
2021-08-25 15:41:05 +0300

    DZ

---
+++ src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+public class Main {
+    private static final int CURRENT_YEAR = 2021;
+
+    private String surname;
+    private String secondName;
+    private String name;
+    private String position;
+    private String phone;
+    private int salary;
+    private int birthYear;
+
+
+    public Main(String name,String secondName,
+                String surname,
+                String phone,
+                String position,
+                int salary,
+                int birthYear) {
+        this.birthYear = birthYear;
+        this.name = name;
+        this.surname = surname;
+        this.secondName = secondName;
+        this.position = position;
+        this.phone = phone;
+        this.salary = salary;
+    }
+
+
+    int getAge() {
+        return CURRENT_YEAR - birthYear;
+    }
+
+    int getSalary() {
+        return salary;
+    }
+
+    String getSecondName() {
+        return secondName;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getSurname() {
+        return surname;
+    }
+
+    String getPosition() {
+        return position;
+    }
+
+    String getPhone() {
+        return phone;
+    }
+
+    String getFullInfo() {
+        return this.name + " " +
+                this.secondName + " " +
+                this.surname + ", " +
+                this.getAge() + " years old, " +
+                this.position + ". Phone number: " +
+                this.phone + ". Salary is " +
+                this.getSalary() + " RU";
+    }
+}
